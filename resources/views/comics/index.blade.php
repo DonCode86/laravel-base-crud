be2ec2048b8cbac645250e8482404ac4827932ea
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-07-14 19:31:20 +0200

    stampa fumetti

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,5 +6,28 @@
 
 @section('page-content')
   <h1>Tutti i fumetti</h1>
-  @dump($comics)
+  <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Immagine</th>
+      <th scope="col">Titolo</th>
+      <th scope="col">Prezzo</th>
+      <th scope="col">Data di vendita</th>
+      <th scope="col">Azioni</th>
+    </tr>
+  </thead>
+  <tbody>
+      @foreach ($comics as $comic)
+        <tr>
+          <td>{{$comic->id}}</td>
+          <td><img src="{{$comic->thumb}}"></td>
+          <td>{{$comic->title}}</td>
+          <td>{{$comic->price}}</td>
+          <td>{{$comic->sale_date}}</td>
+          <td></td>
+        </tr>
+      @endforeach
+  </tbody>
+</table>
 @endsection
