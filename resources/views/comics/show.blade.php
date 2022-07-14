add535dfc2ef088d20dfb753d650b97b8b70b299
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-07-14 20:36:28 +0200

    aggiunta stile

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,9 +5,13 @@
 @endsection
 
 @section('page-content')
-  <img src="{{$comic->thumb}}" alt="">
-  <h1>{{$comic->title}}</h1>
-  <div>
-    <p>{{!!$comic->description!!}}</p>
+  <div class="show-section">
+    <a class="btn btn-outline-dark" href="{{route('comics.index')}}">Torna alla Home</a>
+    <img src="{{$comic->thumb}}" alt="">
+    <h1 class="text-center">{{$comic->title}}</h1>
+    <h5 class="text-center">Genere: "{{$comic->type}}"</h5>
+    <div>
+      <p class="my-3">{{!!$comic->description!!}}</p>
+    </div>
   </div>
 @endsection
