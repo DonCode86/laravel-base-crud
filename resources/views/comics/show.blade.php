55a2284b34bd7a2b70cc762db692ac7f3edc24e0
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-07-15 17:25:49 +0200

    aggiunto pulsante modifica allo show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,6 +7,7 @@
 @section('page-content')
   <div class="show-section">
     <a class="btn btn-outline-dark" href="{{route('comics.index')}}">Torna alla Home</a>
+    <a class="btn btn-outline-danger" href="{{route('comics.edit', $comic->id)}}">Modifica</a>
     <img src="{{$comic->thumb ? $comic->thumb : 'https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01'}}" alt="">
     <h1 class="text-center">{{$comic->title}}</h1>
     <h5 class="text-center">Genere: "{{$comic->type}}"</h5>
